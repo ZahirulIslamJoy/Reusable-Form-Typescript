6bd7df5c1af94ea25a0a6f6caeaf680ab3a5c62f
ZahirulIslamJoy <neloy1613@gmail.com>
2024-02-28 17:06:39 +0600

    added the zod validation

--- src/components/NormalForm.tsx
+++ src/components/NormalForm.tsx
@@ -1,8 +1,26 @@
+import {FieldValues, useForm} from "react-hook-form"
+import { z } from "zod";
+import {zodResolver} from "@hookform/resolvers/zod"
 const NormalForm = () => {
   const double = true;
+  const signUpSchema=z.object({
+    name:z.string().min(1,"Name is required"),
+    email:z.string().email().min(1,"Email is required"),
+    phone:z.string().min(1,"Phone is required"),
+    password:z.string().min(8,"Password is too short")
+  })
+
+  type TForm=z.infer<typeof signUpSchema>
+
+  const {register, handleSubmit, formState: { errors },}=useForm<TForm>({
+    resolver:zodResolver(signUpSchema)
+  });
+  const onSubmit=(data:FieldValues)=>{
+    console.log(data);
+  }
 
   return (
-    <form
+    <form onSubmit={handleSubmit(onSubmit)}
       className={`w-full  border border-gray-300  shadow-sm ${
         double ? "max-w-5xl" : "max-w-md"
       } p-6 mx-auto`}
@@ -14,19 +32,23 @@ const NormalForm = () => {
       >
         <div className="w-full max-w-md">
           <label htmlFor="">Name</label>
-          <input type="text" id="name" name="name" />
+          <input type="text" id="name"  {...register("name")} />
+          {errors.name && (<p className="text-xs text-red-600">{errors.name.message}</p>)}
         </div>
         <div className="w-full max-w-md">
           <label htmlFor="">Email</label>
-          <input type="text" id="name" name="email" />
+          <input type="text" id="email"  {...register("email")} />
+          {errors.email && (<p className="text-xs text-red-600">{errors.email.message}</p>)}
         </div>
         <div className="w-full max-w-md">
           <label htmlFor="">Phone No</label>
-          <input type="text" id="phone" name="phone" />
+          <input type="text" id="phone"  {...register("phone")}  />
+          {errors.phone && (<p className="text-xs text-red-600">{errors.phone.message}</p>)}
         </div>
         <div className="w-full max-w-md">
           <label htmlFor="">Password</label>
-          <input type="text" id="password" name="password" />
+          <input type="text" id="password" {...register("password")}  />
+          {errors.password && (<p className="text-xs text-red-600">{errors.password.message}</p>)}
         </div>
       </div>
       <div
