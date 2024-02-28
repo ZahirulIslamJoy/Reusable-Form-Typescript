ff684a598871dd7daa930056e8467716f49b8526
ZahirulIslamJoy <neloy1613@gmail.com>
2024-02-28 16:28:40 +0600

    completed the design of reusable form

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,6 +1,6 @@
 const Container: React.FC<{ children: React.ReactNode }> = ({children}) => {
     return (
-        <div className="max-w-[1250px] mx-auto">
+        <div className="max-w-[1250px] mx-auto mt-12">
             {children}
         </div>
     );
--- src/components/NormalForm.tsx
+++ src/components/NormalForm.tsx
@@ -1,22 +1,43 @@
 const NormalForm = () => {
+  const double = true;
+
   return (
-    <form className="border border-red-700 max-w-5xl p-5 mx-auto">
-      <div className="border border-green-500">
-        <div>
+    <form
+      className={`w-full  border border-gray-300  shadow-sm ${
+        double ? "max-w-5xl" : "max-w-md"
+      } p-6 mx-auto`}
+    >
+      <div
+        className={` grid grid-cols-1 justify-items-center  gap-5 ${
+          double ? "md:grid-cols-2" : "grid-cols-1"
+        }`}
+      >
+        <div className="w-full max-w-md">
           <label htmlFor="">Name</label>
-          <input className="block" type="text" id="name" name="name" />
+          <input type="text" id="name" name="name" />
         </div>
-        <div>
+        <div className="w-full max-w-md">
           <label htmlFor="">Email</label>
-          <input className="block" type="text" id="name" name="email" />
+          <input type="text" id="name" name="email" />
         </div>
-        <div>
+        <div className="w-full max-w-md">
           <label htmlFor="">Phone No</label>
-          <input className="block" type="text" id="phone" name="phone" />
+          <input type="text" id="phone" name="phone" />
         </div>
-        <div>
+        <div className="w-full max-w-md">
           <label htmlFor="">Password</label>
-          <input className="block" type="text" id="password" name="password" />
+          <input type="text" id="password" name="password" />
+        </div>
+      </div>
+      <div
+        className={` grid grid-cols-1 mt-6 justify-items-center  gap-5 ${
+          double ? "md:grid-cols-2" : "grid-cols-1"
+        }`}
+      >
+        <div className={`w-full max-w-md col-start-1 ${double?"md:col-start-2 flex  md:justify-end":''}`}>
+          <button className="px-3 py-1 bg-violet-600 hover:bg-violet-700  rounded-lg text-white">
+            Submit
+          </button>
         </div>
       </div>
     </form>
